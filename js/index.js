c0af54e9180314b7f57dd5ed532d3afbcc0fe482
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-09-08 02:39:42 +0300

    Add the computational segment for the calculation of the day and determination of Akan name.

--- js/index.js
+++ js/index.js
@@ -42,3 +42,71 @@ function validation()
         return true;
     }
 }
+
+function day(century, year, month, date)
+{
+    return ((((century/4)-2*century-1)+(5*year/4) + (26*(month+1)/10) + date)%7);
+}
+
+function akanFinder()
+{
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thurday", "Friday", "Saturday"];
+    var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var femaleName = ["Akosua", "Adwea", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+    var yearValue = document.getElementById("yearofbirth").value;
+
+    var century = (yearValue.slice(0,2));
+    var year = (yearValue.slice(2,4));
+    var month = document.getElementById("monthofbirth").value;
+    var date = document.getElementById("dateofbirth").value;
+    var gender = document.getElementById("gender").value;
+
+    var result=day(century,year,month,date).toFixed(); 
+
+    if (gender == "1" && result == 0) {
+        swal("you were born on " +days[0]+ " and your akan name is " +maleName[0]);
+        event.preventDefault();
+      } else if (gender == "2" && result == 0) {
+        swal("you were born on " +days[0]+ " and your akan name is " +femaleName[0]);
+        event.preventDefault();
+      }else if (gender == "1" && result == 1) {
+        swal("you were born on " +days[1]+ " and your akan name is " +maleName[1]);
+        event.preventDefault();
+      } else if (gender == "2" && result == 1) {
+        swal("you were born on " +days[1]+ " and your akan name is " +femaleName[1]);
+        event.preventDefault();
+      }else if (gender == "1" && result == 2) {
+        swal("you were born on " +days[2]+ " and your akan name is " +maleName[2]);
+        event.preventDefault();
+      } else if (gender == "2" && result == 2) {
+        swal("you were born on " +days[2]+ " and your akan name is " +femaleName[2]);
+        event.preventDefault();
+      }else if (gender == "1" && result == 3) {
+        swal("you were born on " +days[3]+ " and your akan name is " +maleName[3]);
+        event.preventDefault();
+      } else if (gender == "2" && result == 3) {
+        swal("you were born on " +days[3]+ " and your akan name is " +femaleName[3]);
+        event.preventDefault();
+      }else if (gender == "1" && result == 4) {
+        swal("you were born on " +days[4]+ " and your akan name is " +maleName[4]);
+        event.preventDefault();
+      } else if (gender == "2" && result == 4) {
+        swal("you were born on " +days[4]+ " and your akan name is " +femaleName[4]);
+        event.preventDefault();
+      }else if (gender == "1" && result == 5) {
+        swal("you were born on " +days[5]+ " and your akan name is " +maleName[5]);
+        event.preventDefault();
+      } else if (gender == "2" && result == 5) {
+        swal("you were born on " +days[5]+ " and your akan name is " +femaleName[5]);
+        event.preventDefault();
+      }else if (gender == "1" && result == 6) {
+        swal("you were born on " +days[6]+ " and your akan name is " +maleName[6]);
+        event.preventDefault();
+      } else if (gender == "2" && result == 6) {
+        swal("you were born on " +days[6]+ " and your akan name is " +femaleName[6]);
+        event.preventDefault();
+      }
+      
+    
+}
